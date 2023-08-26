32e26742759e6f69b02cf64e0369afce270e4057
Pedro-Foramilio <phcfor@gmail.com>
2023-08-26 00:40:04 -0300

    checkpoin

--- Xadrez/src/Lib.hs
+++ Xadrez/src/Lib.hs
@@ -1,58 +1,8 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib ( someFunc ) where
 
 import TiposBase
 
--- 1. Coletar Input do Usuario: devolver Posicao ini Posicao fin
--- 2.1 Validar o movimento da peca (origem x destino)
--- 2.2 Validar Excecoes (ROque, Cheque, Cheque Mate, En Passan)
--- 3.1 Atualizar o tabuleiro de acordo
--- 3.2 Retornar erro e pedir input novamente -> 2.
--- 4. Atualizar estado para sinalizar movimento do proximo jogador
-
-
-{--
-initialBoard :: Board
-initialBoard =
-  [ [ Occupied Black Rook, Occupied Black Knight, Occupied Black Bishop, Occupied Black Queen, Occupied Black King, Occupied Black Bishop, Occupied Black Knight, Occupied Black Rook ]
-  , [ Occupied Black Pawn, Occupied Black Pawn,   Occupied Black Pawn,   Occupied Black Pawn,  Occupied Black Pawn, Occupied Black Pawn,   Occupied Black Pawn,   Occupied Black Pawn ]
-  , [ Empty,               Empty,                 Empty,                 Empty,                Empty,               Empty,                 Empty,                 Empty               ]
-  , [ Empty,               Empty,                 Empty,                 Empty,                Empty,               Empty,                 Empty,                 Empty               ]
-  , [ Empty,               Empty,                 Empty,                 Empty,                Empty,               Empty,                 Empty,                 Empty               ]
-  , [ Empty,               Empty,                 Empty,                 Empty,                Empty,               Empty,                 Empty,                 Empty               ]
-  , [ Occupied White Pawn, Occupied White Pawn,   Occupied White Pawn,   Occupied White Pawn,  Occupied White Pawn, Occupied White Pawn,   Occupied White Pawn,   Occupied White Pawn ]
-  , [ Occupied White Rook, Occupied White Knight, Occupied White Bishop, Occupied White Queen, Occupied White King, Occupied White Bishop, Occupied White Knight, Occupied White Rook ]
-  ]
-
-renderBoard :: Board -> IO ()
-renderBoard board = putStrLn $ unlines $ map renderRow board
-  where
-    renderSquare :: Square -> [Char]
-    renderSquare Empty                      = "-"
-    renderSquare (Occupied piece)     = renderPiece color piece
-
-    renderPiece :: Color -> Piece -> [Char]
-    renderPiece Black King     = "♚"
-    renderPiece Black Queen    = "♛"
-    renderPiece Black Rook     = "♜"
-    renderPiece Black Bishop   = "♝"
-    renderPiece Black Knight   = "♞"
-    renderPiece Black Pawn     = "♟"
-    renderPiece White King     = "♔"
-    renderPiece White Queen    = "♕"
-    renderPiece White Rook     = "♖"
-    renderPiece White Bishop   = "♗"
-    renderPiece White Knight   = "♘"
-    renderPiece White Pawn     = "♙"
-
-    renderRow :: [Square] -> [Char]
-    renderRow = concatMap renderSquare
-
-
 someFunc :: IO ()
-someFunc = renderBoard initialBoard
---}
+someFunc = do
+   print $ show $ validaMovimento (Bishop White) (Position 'A' 1)  (Position 'H' 8)
 
-someFunc :: IO ()
-someFunc = print "oi"
--- Xadrez/test/Spec.hs
+++ Xadrez/test/Spec.hs
@@ -1,2 +1,103 @@
+import           Test.Tasty
+import           Test.Tasty.HUnit
+
+import TiposBase
+
+pBranco = Pawn White
+pPreto  = Pawn Black
+nBranco = Knight White
+nPreto = Knight Black
+bBranco = Bishop White
+bPreto = Bishop Black
+rBranco = Rook White
+rPreto = Rook Black
+qBranco = Queen White
+qPreto = Queen Black
+kBranco = King White
+kPreto = King Black
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Testes Relevantes" [testePeaoInicioFim, testeCavaloInicioFim, testeBispoInicioFim]
+
+testePeaoInicioFim = testGroup
+            "Jogadas Peoes - Coordenadas"
+            [
+                testCase
+                    "Peao Branco B2 B4"
+                    (assertEqual "" True (validaMovimento pBranco (Position 'B' 2) (Position 'B' 4)))
+                , testCase
+                    "Peao Branco C2 C3"
+                    (assertEqual "" True (validaMovimento pBranco (Position 'C' 2) (Position 'C' 3)))
+                , testCase
+                    "Peao Branco C8 C7"
+                    (assertEqual "" False (validaMovimento pBranco (Position 'C' 8) (Position 'C' 7)))
+                , testCase
+                    "Peao Branco C4 B5"
+                    (assertEqual "" False (validaMovimento pBranco (Position 'C' 4) (Position 'B' 5)))
+                , testCase
+                    "Peao Preto D7 D5"
+                    (assertEqual "" True (validaMovimento pPreto (Position 'D' 7) (Position 'D' 5)))
+                , testCase
+                    "Peao Preto E7 E6"
+                    (assertEqual "" True (validaMovimento pPreto (Position 'E' 7) (Position 'E' 6)))
+                , testCase
+                    "Peao Branco A2 A3"
+                    (assertEqual "" False (validaMovimento pPreto (Position 'A' 2) (Position 'A' 3)))
+                , testCase
+                    "Peao Branco C5 B4"
+                    (assertEqual "" False (validaMovimento pPreto (Position 'C' 5) (Position 'B' 4)))
+            ]
+
+testeCavaloInicioFim = testGroup
+            "Jogadas Cavalo - Coordenadas"
+            [
+                testCase
+                    "Cavalo C2 D4"
+                    (assertEqual "" True (validaMovimento nBranco (Position 'C' 2) (Position 'D' 4)))
+                , testCase
+                    "Cavalo D2 F3"
+                    (assertEqual "" True (validaMovimento nPreto (Position 'D' 2) (Position 'F' 3)))
+                , testCase
+                    "Cavalo G7 H9"
+                    (assertEqual "" False (validaMovimento nBranco (Position 'G' 7) (Position 'H' 9)))
+                , testCase
+                    "Cavalo F4 G2"
+                    (assertEqual "" True (validaMovimento nPreto (Position 'F' 4) (Position 'G' 2)))
+                , testCase
+                    "Cavalo E3 C2"
+                    (assertEqual "" True (validaMovimento nBranco (Position 'E' 3) (Position 'C' 2)))
+                , testCase
+                    "Cavalo G7 H6"
+                    (assertEqual "" False (validaMovimento nPreto (Position 'G' 7) (Position 'H' 6)))
+                , testCase
+                    "Cavalo F5 G4"
+                    (assertEqual "" False (validaMovimento nBranco (Position 'F' 5) (Position 'G' 4)))
+            ]
+
+testeBispoInicioFim = testGroup
+            "Jogadas Bispo - Coordenadas"
+            [
+                testCase
+                    "Bispo D4 F6"
+                    (assertEqual "" True (validaMovimento bBranco (Position 'D' 4) (Position 'F' 6)))
+                , testCase
+                    "Bispo E4 B1"
+                    (assertEqual "" True (validaMovimento bPreto (Position 'E' 4) (Position 'B' 1)))
+                , testCase
+                    "Bispo F1 A6"
+                    (assertEqual "" True (validaMovimento bBranco (Position 'F' 1) (Position 'A' 6)))
+                , testCase
+                    "Bispo A8 H1"
+                    (assertEqual "" True (validaMovimento bPreto (Position 'A' 8) (Position 'H' 1)))
+                , testCase
+                    "Bispo A8 H2"
+                    (assertEqual "" False (validaMovimento bPreto (Position 'A' 8) (Position 'H' 2)))
+                , testCase
+                    "Bispo F1 G3"
+                    (assertEqual "" False (validaMovimento bPreto (Position 'F' 1) (Position 'G' 3)))
+            ]
